2c55fb5c96449e23ea959c43ce31daf1b866bcc5
Lars Muldjord <muldjordlars@gmail.com>
2022-02-28 15:20:31 +0100

    Updated about and license tabs

--- src/about.cpp
+++ src/about.cpp
@@ -28,41 +28,34 @@
 #include "about.h"
 
 #include <stdio.h>
-#include <QVBoxLayout>
-#include <QLabel>
-#include <QScrollArea>
+#include <QTextEdit>
 #include <QFile>
 
 About::About(QWidget *parent) : QTabWidget(parent)
 {
   setMinimumWidth(600);
+  setStyleSheet("QTextEdit {background-color: white; padding: 15px;}");
 
-  QLabel *aboutText = new QLabel(tr("<strong>VisuTest v" VERSION" by Lars Bisballe</strong><br/><br/>VisuTest is a visual acuity test software designed for use on the RaspBerry Pi hardware platform. It enables you to built an affordable visual acuity test system using a few pieces of hardware and basically any remote control. If you do not have the option to buy the infrared reciever, it can also be used with a keyboard.<br/><br/>Please note that VisuTest is provided without fonts. These need to be acquired elsewhere."));
-  aboutText->setWordWrap(true);
-  aboutText->setMaximumWidth(400);
+  QTextEdit *aboutText = new QTextEdit(tr("<img src=\":icon.png\"/><h1>VisuTest v" VERSION"</h1><h3>by Lars Bisballe</h3><h4>https://www.github.com/muldjord/visutest</h4><p>VisuTest is a visual acuity test software designed for use on the RaspBerry Pi hardware platform.It enables you to built an affordable visual acuity test system using a few pieces of hardware and basically any remote control. If you do not have the option to buy the infrared reciever, it can also be used with a keyboard.</p><h4>General keyboard functions</h4><p><ul><li>q: Hibernate / turn monitor off</li><li>u: Launch updater (requires pin-code)</li><li>p: Launch preferences (requires pin-code)</li></ul></p><h4>Optotype chart specific keyboard functions</h4><ul><li>up: Change to next line with bigger size</li><li>down: Change to next line with smaller size</li><li>left: Move entire row to the left</li><li>right: Move entire row to the right</li><li>w: Skip n number of lines bigger (configure n in preferences)</li><li>s: Skip n number of lines smaller (configure n in preferences)</li><li>c: Enable / disable crowding</li><li>m: Switch between single and multi-optotype from the selected row. Use left / right to move between them</li><li>r: Randomize optotypes from selected line</li><li>v: Enable / disable attention video or gif animation</li></ul></p><h4>SVG chart specific keyboard functions</h4><p><ul><li>left / right: Switch between layers defined in charts.xml</li><li></li><li></li><li></li><li></li><li></li><li></li><ul></p><p>Using a remote control reciever that emulates a keyboard, it is possible to control VisuTest with basically any remote control. The Flirc USB has been tested to work well: https://flirc.tv/more/flirc-usb"));
+  aboutText->setWordWrapMode(QTextOption::WordWrap);
+  aboutText->setReadOnly(true);
 
-  // License tab
-  // Read COPYING data from file
-  QFile file("COPYING");
-  QByteArray gplText;
+  QFile file("LICENSE");
+  QByteArray gplText = "";
   if(file.open(QIODevice::ReadOnly)) {
     gplText = file.readAll();
     file.close();
   } else {
-    printf("ERROR: Couldn't find COPYING file at the designated location.\n");
-    gplText = "ERROR: File not found... This means that someone has been fiddling with the files of this software, and someone might be violating the terms of the GPL. Go to the following location to read the license: http://www.gnu.org/licenses/gpl-3.0.html";
+    printf("ERROR: Couldn't find LICENSE file at the designated location.\n");
+    gplText = "ERROR: File not found... This means that someone has been fiddling with the files of this software and someone might be violating the terms of the GPL. Go to the following location to read the license: http://www.gnu.org/licenses/gpl-3.0.html";
   }
 
-  QLabel *licenseText = new QLabel(gplText);
-  licenseText->setWordWrap(true);
-  licenseText->setMaximumWidth(400);
-
-  QScrollArea *licenseScroll = new QScrollArea;
-  licenseScroll->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-  licenseScroll->setWidget(licenseText);
-
+  QTextEdit *licenseText = new QTextEdit(gplText);
+  licenseText->setWordWrapMode(QTextOption::WordWrap);
+  //licenseText->setStyleSheet("QTextEdit {background-color: white; padding: 15px;}");
+  
   addTab(aboutText, tr("About"));
-  addTab(licenseScroll, tr("License"));
+  addTab(licenseText, tr("License"));
 }
 
 About::~About()
