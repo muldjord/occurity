5473f311041371ecc974623983a50ef36c717f2b
Lars Muldjord <muldjordlars@gmail.com>
2022-03-21 12:56:29 +0100

    Updated

--- visutest_da_DK.ts
+++ visutest_da_DK.ts
@@ -4,16 +4,20 @@
 <context>
     <name>About</name>
     <message>
-        <source>&lt;strong&gt;VisuTest v</source>
-        <translation>&lt;strong&gt;VisuTest v</translation>
-    </message>
-    <message>
         <source>About</source>
         <translation>Om</translation>
     </message>
     <message>
         <source>License</source>
         <translation>Licens</translation>
+    </message>
+    <message>
+        <source>&lt;h1&gt;VisuTest v</source>
+        <translation>&lt;h1&gt;VisuTest v</translation>
+    </message>
+    <message>
+        <source>&lt;/h1&gt;&lt;h3&gt;by Lars Bisballe&lt;/h3&gt;&lt;h4&gt;https://www.github.com/muldjord/visutest&lt;/h4&gt;&lt;p&gt;VisuTest is a visual acuity test software designed for use on the RaspBerry Pi hardware platform.It enables you to built an affordable visual acuity test system using a few pieces of hardware and basically any remote control. If you do not have the option to buy the infrared reciever, it can also be used with a keyboard.&lt;/p&gt;&lt;h4&gt;General keyboard functions&lt;/h4&gt;&lt;p&gt;&lt;ul&gt;&lt;li&gt;q: Hibernate / turn monitor off&lt;/li&gt;&lt;li&gt;u: Launch updater (requires pin-code)&lt;/li&gt;&lt;li&gt;p: Launch preferences (requires pin-code)&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;h4&gt;Optotype chart specific keyboard functions&lt;/h4&gt;&lt;ul&gt;&lt;li&gt;up: Change to next line with bigger size&lt;/li&gt;&lt;li&gt;down: Change to next line with smaller size&lt;/li&gt;&lt;li&gt;left: Move entire row to the left&lt;/li&gt;&lt;li&gt;right: Move entire row to the right&lt;/li&gt;&lt;li&gt;PgUp: Skip n number of lines bigger (configure n in preferences)&lt;/li&gt;&lt;li&gt;PgDn: Skip n number of lines smaller (configure n in preferences)&lt;/li&gt;&lt;li&gt;c: Enable / disable crowding&lt;/li&gt;&lt;li&gt;m: Switch between single and multi-optotype from the selected row. Use left / right to move between them&lt;/li&gt;&lt;li&gt;r: Randomize optotypes from selected line&lt;/li&gt;&lt;li&gt;a: Enable / disable attention gif animation&lt;/li&gt;&lt;li&gt;z: Start attention video&lt;/li&gt;&lt;li&gt;x: Stop attention video&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;h4&gt;SVG chart specific keyboard functions&lt;/h4&gt;&lt;p&gt;&lt;ul&gt;&lt;li&gt;left / right: Switch between layers defined in charts.xml&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;ul&gt;&lt;/p&gt;&lt;p&gt;Using a remote control reciever that emulates a keyboard, it is possible to control VisuTest with basically any remote control. The Flirc USB has been tested to work well: https://flirc.tv/more/flirc-usb</source>
+        <translation>&lt;/h1&gt;&lt;h3&gt;af Lars Bisballe&lt;/h3&gt;&lt;h4&gt;https://www.github.com/muldjord/visutest&lt;/h4&gt;&lt;p&gt;VisuTest er et synstestsystem designet til brug med Raspberry Pi hardware platformen. Dette tillader enhver at bygge et system til synstest ved brug af nogle lettilgængelige hardware enheder og en generisk fjernbetjening. Hvis ikke du har mulighed for at indkøbe IR modtageren, kan systemet også bruges med et tastatur.&lt;/p&gt;&lt;h4&gt;Generelle tastatur funktioner&lt;/h4&gt;&lt;p&gt;&lt;ul&gt;&lt;li&gt;q: Start / stop dvale for skærmen&lt;/li&gt;&lt;li&gt;u: Åbn opdateringsdialog (kræver pin-kode)&lt;/li&gt;&lt;li&gt;p: Åben konfigurationsdialog (kræver pin-kode)&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;h4&gt;Funtionker specifikke for optotype diagram&lt;/h4&gt;&lt;ul&gt;&lt;li&gt;Op: Skift til næste større linie&lt;/li&gt;&lt;li&gt;Ned: Skift til næste mindre linie&lt;/li&gt;&lt;li&gt;Venstre: Flyt viste række mod venstre&lt;/li&gt;&lt;li&gt;Højre: Flyt viste række mod højre&lt;/li&gt;&lt;li&gt;PgUp: Spring n rækker over i større retning (opsæt n i konfigurationsdialogen)&lt;/li&gt;&lt;li&gt;PgDn: Spring n rækker over i mindre retning (opsæt n i konfigurationsdialogen)&lt;/li&gt;&lt;li&gt;c: Slå crowding til / fra&lt;/li&gt;&lt;li&gt;m: Skift mellem enkelt og flertegnsvisning for den viste række. Brug højre / venstre for at skifte mellem dem&lt;/li&gt;&lt;li&gt;r: Bland optotyper fra den aktuelle linie&lt;/li&gt;&lt;li&gt;a: Slå opmærksomhedsanimation til / fra&lt;/li&gt;&lt;li&gt;z: Start opmærksomhedsvideo&lt;/li&gt;&lt;li&gt;x: Stop opmærksomhedsvideo&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;h4&gt;Funktioner specifikke for SVG diagram&lt;/h4&gt;&lt;p&gt;&lt;ul&gt;&lt;li&gt;Venstre / Højre: Skift mellem SVG-lag defineret i charts.xml&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;ul&gt;&lt;/p&gt;&lt;p&gt;Ved brug af en fjernbetjeningsmodtager der emulerer et tastatur, er det muligt at kontrollere VisuTest med praktisk talt alle fjernbetjeninger. Flirc USB-enheden er testet og fungerer godt: https://flirc.tv/more/flirc-usb</translation>
     </message>
 </context>
 <context>
@@ -29,6 +33,13 @@
     <message>
         <source>&apos; not found. Please correct this.</source>
         <translation>&apos; blev ikke fundet. Ret venligst dette.</translation>
+    </message>
+</context>
+<context>
+    <name>PinDialog</name>
+    <message>
+        <source>Pincode:</source>
+        <translation>Pinkode:</translation>
     </message>
 </context>
 <context>
@@ -59,7 +70,70 @@
     </message>
     <message>
         <source>Skip this many lines when using multiline row skipping:</source>
-        <translation type="unfinished">Antal linier der skal springes over når der bruges flerlinie skifte:</translation>
+        <translation>Antal linier der skal springes over når der bruges flerlinie skifte:</translation>
+    </message>
+</context>
+<context>
+    <name>Updater</name>
+    <message>
+        <source>VisuTest updater</source>
+        <translation>VisuTest opdatering</translation>
+    </message>
+    <message>
+        <source>Apply update from update folder</source>
+        <translation>Pålæg opdatering fra update folder</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fejl</translation>
+    </message>
+    <message>
+        <source>The update file</source>
+        <translation>Updateringsfilen</translation>
+    </message>
+    <message>
+        <source>does not exist. Can&apos;t update, aborting!</source>
+        <translation>eksisterer ikke. Opdatering afbrydes!</translation>
+    </message>
+    <message>
+        <source>The apt install command failed. See &apos;debug.log&apos; for more information. Update cancelled!</source>
+        <translation>Apt install kommandoen fejlen. Se &apos;debug.log&apos; for yderligere information. Opdatering afbrudt!</translation>
+    </message>
+    <message>
+        <source>Apt QProcess did not complete with &apos;QProcess::NormalExit&apos; status. Update cancelled!</source>
+        <translation>Apt QProcess afsluttede ikke med &apos;QProcess::NormalExit&apos; status. Opdatering afbrudt!</translation>
+    </message>
+    <message>
+        <source>Destination file could not be removed before copy. Update cancelled!</source>
+        <translation>Destinationsfilen kunne ikke fjernes før kopiering. Opdatering afbrudt!</translation>
+    </message>
+    <message>
+        <source>File copy failed. Update cancelled!</source>
+        <translation>Filkopiering fejlede. Updatering afbrudt!</translation>
+    </message>
+    <message>
+        <source>Path &apos;</source>
+        <translation>Stien &apos;</translation>
+    </message>
+    <message>
+        <source>&apos; does not exist. Update cancelled!</source>
+        <translation>&apos; eksisterer ikke. Opdatering afbrudt!</translation>
+    </message>
+    <message>
+        <source>Path could not be created. Update cancelled!</source>
+        <translation>Stien kunne ikke oprettes. Opdatering afbrudt!</translation>
+    </message>
+    <message>
+        <source>Update completed</source>
+        <translation>Opdatering gennemført</translation>
+    </message>
+    <message>
+        <source>Update completed successfully!
+
+Press OK to reboot the system.</source>
+        <translation>Opdateringen gennemført med succes!
+
+Klik OK for at genstarte systemet.</translation>
     </message>
 </context>
 </TS>
