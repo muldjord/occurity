ac2a27f694dbfb74203ecd311ab9d574ff554d30
Lars Muldjord <muldjordlars@gmail.com>
2022-04-12 10:47:56 +0200

    New optotype symbol class, reused from letter

---
+++ src/optosymbol.cpp
@@ -0,0 +1,80 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/***************************************************************************
+ *            optosymbol.cpp
+ *
+ *  Tue Mar 13 17:00:00 UTC+1 2018
+ *  Copyright 2018 Lars Bisballe
+ *  larsbjensen@gmail.com
+ ****************************************************************************/
+
+/*
+ *  This file is part of Occurity.
+ *
+ *  Occurity is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Occurity is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Occurity; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ */
+
+#include "optosymbol.h"
+
+#include <stdio.h>
+
+OptoSymbol::OptoSymbol(const QString &optoSymbol) : QGraphicsSvgItem(optoSymbol)
+{
+  opacityEffect = new QGraphicsOpacityEffect;
+  opacityEffect->setOpacity(0.0);
+  setGraphicsEffect(opacityEffect);
+
+  fadeInAnimation = new QPropertyAnimation(opacityEffect, "opacity");
+  fadeInAnimation->setDuration(10);
+  //fadeInAnimation->setStartValue(0.0);
+  fadeInAnimation->setEndValue(1.0);
+  fadeInAnimation->setEasingCurve(QEasingCurve::InOutQuad);
+
+  fadeOutAnimation = new QPropertyAnimation(opacityEffect, "opacity");
+  fadeOutAnimation->setDuration(150);
+  fadeOutAnimation->setStartValue(1.0);
+  fadeOutAnimation->setEndValue(0.0);
+  fadeOutAnimation->setEasingCurve(QEasingCurve::InOutQuad);
+  connect(fadeOutAnimation, &QPropertyAnimation::finished, this, &OptoSymbol::fadeDoneHide);
+}
+
+OptoSymbol::~OptoSymbol()
+{
+}
+
+void OptoSymbol::fadeIn()
+{
+  show();
+  fadeInAnimation->start();
+}
+
+void OptoSymbol::fadeOut()
+{
+  fadeOutAnimation->start();
+}
+
+void OptoSymbol::fadeDoneHide()
+{
+  hide();
+}
+
+bool OptoSymbol::isHidden()
+{
+  if(!isVisible() ||
+     fadeOutAnimation->state() == QAbstractAnimation::Running ||
+     fadeOutAnimation->state() == QAbstractAnimation::Paused) {
+    return true;
+  }
+  return false;
+}
---
+++ src/optosymbol.h
@@ -0,0 +1,52 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/***************************************************************************
+ *            optosymbol.h
+ *
+ *  Tue Mar 13 17:00:00 UTC+1 2018
+ *  Copyright 2018 Lars Bisballe
+ *  larsbjensen@gmail.com
+ ****************************************************************************/
+
+/*
+ *  This file is part of Occurity.
+ *
+ *  Occurity is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Occurity is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Occurity; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ */
+#ifndef __OCCURITY_OPTOSYMBOL_H__
+#define __OCCURITY_OPTOSYMBOL_H__
+
+#include <QGraphicsSvgItem>
+#include <QPropertyAnimation>
+#include <QGraphicsOpacityEffect>
+
+class OptoSymbol : public QGraphicsSvgItem
+{
+public:
+  OptoSymbol(const QString &optoSymbol);
+  ~OptoSymbol();
+  void fadeIn();
+  void fadeOut();
+  bool isHidden();
+
+private slots:
+  void fadeDoneHide();
+  
+private:
+  QGraphicsOpacityEffect *opacityEffect;
+  QPropertyAnimation *fadeInAnimation;
+  QPropertyAnimation *fadeOutAnimation;
+
+};
+#endif/*__OCCURITY_OPTOSYMBOL_H__*/
