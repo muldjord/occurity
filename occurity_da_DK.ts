096df5a17263182e470842b7319a02af4946ae3f
Lars Muldjord <muldjordlars@gmail.com>
2024-05-08 11:21:12 +0200

    Updated translations

--- occurity_da_DK.ts
+++ occurity_da_DK.ts
@@ -16,8 +16,8 @@
         <translation>&lt;h1&gt;Occurity v</translation>
     </message>
     <message>
-        <source>&lt;/h1&gt;&lt;h3&gt;by Lars Bisballe&lt;/h3&gt;&lt;h4&gt;https://www.github.com/muldjord/occurity&lt;/h4&gt;&lt;p&gt;Occurity is a visual acuity test software designed for use on the RaspBerry Pi hardware platform.It enables you to built an affordable visual acuity test system using a few pieces of hardware and basically any remote control. If you do not have the option to buy the infrared reciever, it can also be used with a keyboard.&lt;/p&gt;&lt;h4&gt;General keyboard functions&lt;/h4&gt;&lt;p&gt;&lt;ul&gt;&lt;li&gt;q: Enable / disable standby&lt;/li&gt;&lt;li&gt;j: Launch job runner (requires pin-code)&lt;/li&gt;&lt;li&gt;p: Launch preferences (requires pin-code)&lt;/li&gt;&lt;li&gt;s: Restart current attention video&lt;/li&gt;&lt;li&gt;d: Play / pause current attention video&lt;/li&gt;&lt;li&gt;f: Stop attention video&lt;/li&gt;&lt;li&gt;g: Play next attention video&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;h4&gt;Optotype chart specific keyboard functions&lt;/h4&gt;&lt;ul&gt;&lt;li&gt;Up: Change to next line with bigger size&lt;/li&gt;&lt;li&gt;Down: Change to next line with smaller size&lt;/li&gt;&lt;li&gt;Left: Move entire row to the left&lt;/li&gt;&lt;li&gt;Right: Move entire row to the right&lt;/li&gt;&lt;li&gt;PgUp: Skip n number of lines bigger (configure n in preferences)&lt;/li&gt;&lt;li&gt;PgDn: Skip n number of lines smaller (configure n in preferences)&lt;/li&gt;&lt;li&gt;c: Enable / disable crowding&lt;/li&gt;&lt;li&gt;m: Switch between single and multisymbol from the selected row. Use left / right to move between them&lt;/li&gt;&lt;li&gt;r: Randomize symbols from selected row&lt;/li&gt;&lt;li&gt;a: Enable / disable attention animation (if one is defined)&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;h4&gt;SVG chart specific keyboard functions&lt;/h4&gt;&lt;p&gt;&lt;ul&gt;&lt;li&gt;left / right: Switch between layers defined in charts.xml&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;ul&gt;&lt;/p&gt;&lt;p&gt;Using a remote control reciever that emulates a keyboard, it is possible to control Occurity with basically any remote control. The Flirc USB has been tested to work well: https://flirc.tv/more/flirc-usb</source>
-        <translation>&lt;/h1&gt;&lt;h3&gt;af Lars Bisballe&lt;/h3&gt;&lt;h4&gt;https://www.github.com/muldjord/occurity&lt;/h4&gt;&lt;p&gt;Occurity er et synstestsystem designet til brug med Raspberry Pi hardware platformen. Dette tillader enhver at bygge et system til synstest ved brug af nogle lettilgængelige hardware enheder og en generisk fjernbetjening. Hvis ikke du har mulighed for at indkøbe IR modtageren, kan systemet også bruges med et tastatur.&lt;/p&gt;&lt;h4&gt;Generelle tastatur funktioner&lt;/h4&gt;&lt;p&gt;&lt;ul&gt;&lt;li&gt;q: Start / stop dvale for skærmen&lt;/li&gt;&lt;li&gt;u: Åbn opgavekørsel dialog (kræver pin-kode)&lt;/li&gt;&lt;li&gt;p: Åben konfigurationsdialog (kræver pin-kode)&lt;/li&gt;&lt;li&gt;s: Genstart nuværende opmærksomhedsvideo&lt;/li&gt;&lt;li&gt;d: Afspil / stop opmærksomhedsvideo&lt;/li&gt;&lt;li&gt;f: Stop opmærksomhedsvideo&lt;/li&gt;&lt;li&gt;g: Afspil næste opmærksomhedsvideo&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;h4&gt;Funtionker specifikke for optotype diagram&lt;/h4&gt;&lt;ul&gt;&lt;li&gt;Op: Skift til næste større linie&lt;/li&gt;&lt;li&gt;Ned: Skift til næste mindre linie&lt;/li&gt;&lt;li&gt;Venstre: Flyt viste række mod venstre&lt;/li&gt;&lt;li&gt;Højre: Flyt viste række mod højre&lt;/li&gt;&lt;li&gt;PgUp: Spring n rækker over i større retning (opsæt n i konfigurationsdialogen)&lt;/li&gt;&lt;li&gt;PgDn: Spring n rækker over i mindre retning (opsæt n i konfigurationsdialogen)&lt;/li&gt;&lt;li&gt;c: Slå crowding til / fra&lt;/li&gt;&lt;li&gt;m: Skift mellem enkelt og flertegnsvisning for den viste række. Brug højre / venstre for at skifte mellem dem&lt;/li&gt;&lt;li&gt;r: Bland optotyper fra den aktuelle linie&lt;/li&gt;&lt;li&gt;a: Slå opmærksomhedsanimation til / fra&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;h4&gt;Funktioner specifikke for SVG diagram&lt;/h4&gt;&lt;p&gt;&lt;ul&gt;&lt;li&gt;Venstre / Højre: Skift mellem SVG-lag defineret i charts.xml&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;ul&gt;&lt;/p&gt;&lt;p&gt;Ved brug af en fjernbetjeningsmodtager der emulerer et tastatur, er det muligt at kontrollere Occurity med praktisk talt alle fjernbetjeninger. Flirc USB-enheden er testet og fungerer godt: https://flirc.tv/more/flirc-usb</translation>
+        <source>&lt;/h1&gt;&lt;h3&gt;by Lars Bisballe&lt;/h3&gt;&lt;h4&gt;https://www.github.com/muldjord/occurity&lt;/h4&gt;&lt;p&gt;Occurity is a visual acuity test software designed for use on the RaspBerry Pi hardware platform. It enables you to built an affordable visual acuity test system using a few pieces of hardware and basically any remote control. If you do not have the option to buy the infrared reciever, it can also be used with a keyboard.&lt;/p&gt;&lt;h4&gt;General keyboard functions&lt;/h4&gt;&lt;p&gt;&lt;ul&gt;&lt;li&gt;q: Enable / disable standby&lt;/li&gt;&lt;li&gt;j: Launch job runner (requires pin-code)&lt;/li&gt;&lt;li&gt;p: Launch preferences (requires pin-code)&lt;/li&gt;&lt;li&gt;s: Restart current attention video&lt;/li&gt;&lt;li&gt;d: Play / pause current attention video&lt;/li&gt;&lt;li&gt;f: Stop attention video&lt;/li&gt;&lt;li&gt;g: Play next attention video&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;h4&gt;Optotype chart specific keyboard functions&lt;/h4&gt;&lt;ul&gt;&lt;li&gt;Up: Change to next line with bigger size&lt;/li&gt;&lt;li&gt;Down: Change to next line with smaller size&lt;/li&gt;&lt;li&gt;Left: Move entire row to the left&lt;/li&gt;&lt;li&gt;Right: Move entire row to the right&lt;/li&gt;&lt;li&gt;PgUp: Skip n number of lines bigger (configure n in preferences)&lt;/li&gt;&lt;li&gt;PgDn: Skip n number of lines smaller (configure n in preferences)&lt;/li&gt;&lt;li&gt;c: Enable / disable crowding&lt;/li&gt;&lt;li&gt;m: Switch between single and multisymbol from the selected row. Use left / right to move between them&lt;/li&gt;&lt;li&gt;r: Randomize symbols from selected row&lt;/li&gt;&lt;li&gt;a: Enable / disable attention animation (if one is defined)&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;h4&gt;SVG chart specific keyboard functions&lt;/h4&gt;&lt;p&gt;&lt;ul&gt;&lt;li&gt;left / right: Switch between layers defined in charts.xml&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;ul&gt;&lt;/p&gt;&lt;p&gt;Using a remote control reciever that emulates a keyboard, it is possible to control Occurity with basically any remote control. The Flirc USB has been tested to work well: https://flirc.tv/more/flirc-usb</source>
+        <translation>&lt;/h1&gt;&lt;h3&gt;af Lars Bisballe&lt;/h3&gt;&lt;h4&gt;https://www.github.com/muldjord/occurity&lt;/h4&gt;&lt;p&gt;Occurity er designet til brug for synstest af patienter via Raspberry Pi hardware platformen. Occurity gør det muligt at bygge en synstavle til en overkommelig pris ved brug af nogle få stykker hardware og næsten enhver fjernbetning. Har du ikke mulighed for at indkøbe den infrarøde modtager, kan Occurity også betjenes med et tastatur.&lt;/p&gt;&lt;h4&gt;Generelle tastatur funktioner&lt;/h4&gt;&lt;p&gt;&lt;ul&gt;&lt;li&gt;q: Tænd / sluk skærmen&lt;/li&gt;&lt;li&gt;j: Åben job runner (kræver pinkode)&lt;/li&gt;&lt;li&gt;p: Åben opsætning (kræver pinkode)&lt;/li&gt;&lt;li&gt;s: Genstart kørende opmærksomhedsvideo&lt;/li&gt;&lt;li&gt;d: Start / pause kørende opmærksomhedsvideo&lt;/li&gt;&lt;li&gt;f: Stop opmærksomhedsvideo&lt;/li&gt;&lt;li&gt;g: Afspil næste opmærksomhedsvideo&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;h4&gt;Optotype tavle specifikke tastatur funktioner&lt;/h4&gt;&lt;ul&gt;&lt;li&gt;Op: Skift til næste større linie&lt;/li&gt;&lt;li&gt;Ned: Skift til næste mindre linie&lt;/li&gt;&lt;li&gt;Venstre: Flyt hele rækken mod venstre&lt;/li&gt;&lt;li&gt;Højre: Flyt hele rækken mod højre&lt;/li&gt;&lt;li&gt;PgUp: Spring n antal linier over i større retning (opsæt n i opsætningen)&lt;/li&gt;&lt;li&gt;PgDn: Spring n antal linier over i mindre retning (opsæt n i opsætningen)&lt;/li&gt;&lt;li&gt;c: Slå crowding til / fra&lt;/li&gt;&lt;li&gt;m: Skift mellem enkelt og multisymbol for den viste række. Brug højre / venstre for at bladre mellem dem.&lt;/li&gt;&lt;li&gt;r: Bland den viste række tilfældigt&lt;/li&gt;&lt;li&gt;a: Slå opmærksomhedsanimation til / fra (hvis der er defineret en)&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;h4&gt;SVG tavle specifikke tastatur funktioner&lt;/h4&gt;&lt;p&gt;&lt;ul&gt;&lt;li&gt;Højre / venstre: Skift mellem lagene defineret i charts.xml&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;li&gt;&lt;/li&gt;&lt;ul&gt;&lt;/p&gt;&lt;p&gt;Ved at gøre brug af en infrarød modtager der emulerer tastatur tryk, er det muligt at kontrollere Occurity ved brug af næsten enhver fjernbetjening. Flirc USB er testet og fungerer perfekt til formålet: https://flirc.tv/more/flirc-usb</translation>
     </message>
 </context>
 <context>
@@ -61,12 +61,99 @@
         <translation>Den fysiske længde af linealen (mm):</translation>
     </message>
     <message>
-        <source>Idle time before size reset (seconds):</source>
-        <translation>Inaktiv tid før nulstilling af størrelse (sekunder):</translation>
-    </message>
-    <message>
         <source>Skip this many lines when using multiline row skipping:</source>
         <translation>Antal linier der skal springes over når der bruges flerlinie skifte:</translation>
+    </message>
+    <message>
+        <source>Starting chart:</source>
+        <translation>Start på tavle:</translation>
+    </message>
+    <message>
+        <source>Idle time before size reset (minutes):</source>
+        <translation>Inaktiv tid før størrelse nulstilles (minutter):</translation>
+    </message>
+    <message>
+        <source>Idle time before putting the monitor to sleep (minutes):</source>
+        <translation>Inaktiv tid før skærmen sendes i standby (minutter):</translation>
+    </message>
+    <message>
+        <source>Use row captions for optotype charts</source>
+        <translation>Brug overskrifter fra rækker for optotype tavler</translation>
+    </message>
+    <message>
+        <source>Show mouse cursor</source>
+        <translation>Vis musemarkøren</translation>
+    </message>
+    <message>
+        <source>Show touch controls (requires restart)</source>
+        <translation>Vis touch funktioner (kræver genstart)</translation>
+    </message>
+    <message>
+        <source>Left handed operator</source>
+        <translation>Venstrehåndsoperatør</translation>
+    </message>
+</context>
+<context>
+    <name>TouchControls</name>
+    <message>
+        <source>Touch controls</source>
+        <translation>Touch funktioner</translation>
+    </message>
+    <message>
+        <source>0</source>
+        <translation>0</translation>
+    </message>
+    <message>
+        <source>1</source>
+        <translation>1</translation>
+    </message>
+    <message>
+        <source>2</source>
+        <translation>2</translation>
+    </message>
+    <message>
+        <source>3</source>
+        <translation>3</translation>
+    </message>
+    <message>
+        <source>4</source>
+        <translation>4</translation>
+    </message>
+    <message>
+        <source>5</source>
+        <translation>5</translation>
+    </message>
+    <message>
+        <source>6</source>
+        <translation>6</translation>
+    </message>
+    <message>
+        <source>7</source>
+        <translation>7</translation>
+    </message>
+    <message>
+        <source>8</source>
+        <translation>8</translation>
+    </message>
+    <message>
+        <source>9</source>
+        <translation>9</translation>
+    </message>
+    <message>
+        <source>UP</source>
+        <translation>OP</translation>
+    </message>
+    <message>
+        <source>DOWN</source>
+        <translation>NED</translation>
+    </message>
+    <message>
+        <source>LEFT</source>
+        <translation>VENSTRE</translation>
+    </message>
+    <message>
+        <source>RIGHT</source>
+        <translation>HØJRE</translation>
     </message>
 </context>
 </TS>
